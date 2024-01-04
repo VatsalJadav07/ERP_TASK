e8ea8e2c993d1f58306b865b369ac6b6aae4f5ed
Jadav Vatsal <102715396+VatsalJadav07@users.noreply.github.com>
2024-01-04 12:59:41 +0530

    Update student.model.js

--- model/student.model.js
+++ model/student.model.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose')
 const validator = require('validator')
 
 const taskSchema = new mongoose.Schema({
-
     name: {
         type: String,
         required: true,
